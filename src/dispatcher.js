40539214eb9b02416cda0631b790ac0b380ba4b7
Jorik Tangelder <j.tangelder@gmail.com>
2014-10-10 22:41:22 +0200

    some docs

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,5 +1,11 @@
 var Dispatcher = require('flux').Dispatcher;
 
+/**
+ * a bit more standardized way to dispatch actions
+ * @param {String} actionType
+ * @param {*} data
+ * @returns {*}
+ */
 Dispatcher.prototype.dispatchAction = function(actionType, data) {
     return this.dispatch({
         actionType: actionType,
--- src/utils/storeMixin.js
+++ src/utils/storeMixin.js
@@ -1,4 +1,8 @@
-module.exports = function(/* args */) {
+/**
+ * mixin to let components listen to stores in a simple way
+ * the component needs to implement `onStoreUpdate` to set the state
+ */
+module.exports = function(/* storeA, storeB, storeC, ... */) {
     var stores = [].slice.call(arguments, 0);
     return {
         componentDidMount: function() {
