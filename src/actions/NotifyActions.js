924f7dc5cfbd47b12bf696fb88fe1e99fd7ec000
Jorik Tangelder <j.tangelder@gmail.com>
2014-10-12 00:36:44 +0200

    refactor

--- src/actions/NotifyActions.js
+++ src/actions/NotifyActions.js
@@ -3,15 +3,6 @@ var dispatch = require('../dispatcher').dispatchAction;
 
 
 module.exports = {
-    loading: function(text) {
-        dispatch(c.NOTIFY_LOADING, { text: text });
-    },
-    loaded: function() {
-        dispatch(c.NOTIFY_LOADED);
-    },
-    alert: function(text) {
-        dispatch(c.NOTIFY_ALERT, { text: text });
-    },
     hide: function() {
         dispatch(c.NOTIFY_HIDE);
     }
--- src/stores/NotifyStore.js
+++ src/stores/NotifyStore.js
@@ -12,46 +12,30 @@ var NotifyStore = new Backbone.Model({
 function alert(text) {
     NotifyStore.set({
         text: text,
-        type: c.NOTIFY_ALERT,
         visible: true,
         closable: true
     });
 }
 
 NotifyStore.dispatchToken = Dispatcher.register(function(payload){
-    var data = payload.data;
     switch(payload.actionType) {
-        case c.NOTIFY_LOADING:
         case c.FLICKR_FIND:
             NotifyStore.set({
-                text: data.text || 'Loading...',
-                type: c.NOTIFY_LOADING,
+                text: 'Loading...',
                 visible: true,
                 closable: false
             });
             break;
 
-        case c.NOTIFY_LOADED:
-        case c.FLICKR_FIND_SUCCESS:
-            if(NotifyStore.get('type') === c.NOTIFY_LOADING) {
-                NotifyStore.set({
-                    visible: false
-                });
-            }
-            break;
-
         case c.NOTIFY_HIDE:
+        case c.FLICKR_FIND_SUCCESS:
             NotifyStore.set({
                 visible: false
             });
             break;
 
-        case c.NOTIFY_ALERT:
-            alert(data.text);
-            break;
-
         case c.FLICKR_FIND_FAIL:
-            alert('Flickr failed.');
+            alert('Loading failed... Please try again.');
             break;
     }
 });
